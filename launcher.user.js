2ac7c834d8be55dd2b9245c1b96bab23b2207b48
rhgsde <rhwerw@yahoo.com>
2015-07-18 02:47:42 +0700

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1268,7 +1268,7 @@ console.log("Running Bot Launcher!");
                 dArc = [],
                 dText = [],
                 lines = [],
-                names = ["NotReallyABot"],
+                names = ["FuckJesus"],
                 originalName = names[Math.floor(Math.random() * names.length)],
                 sessionScore = 0,
                 serverIP = "",
